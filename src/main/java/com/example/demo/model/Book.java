36ecd0a080eda6d83c32a77ada20eb6175e7352a
Bohdan Stratiychuk <vasyahunta@gmail.com>
2024-11-13 20:41:17 +0200

    checkstyling

--- src/main/java/com/example/demo/model/Book.java
+++ src/main/java/com/example/demo/model/Book.java
@@ -8,52 +8,27 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.math.BigDecimal;
 
 @Entity
+@Setter
+@Getter
 @Table(name = "books")
 public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @NotBlank(message = "Title is required")
+    @Column(nullable = false)
     private String title;
-    @NotBlank(message = "Author is required")
+    @Column(nullable = false)
     private String author;
-    @NotBlank(message = "ISBN is required")
     @Column(unique = true, nullable = false)
     private String isbn;
     @DecimalMin(value = "0.0", inclusive = false, message = "Price must be greater than 0")
     private BigDecimal price;
     private String description;
     private String coverImage;
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public void setTitle(@NotBlank(message = "Title is required") String title) {
-        this.title = title;
-    }
-
-    public void setAuthor(@NotBlank(message = "Author is required") String author) {
-        this.author = author;
-    }
-
-    public void setIsbn(@NotBlank(message = "ISBN is required") String isbn) {
-        this.isbn = isbn;
-    }
-
-    public void setPrice(@DecimalMin(value = "0.0",
-            inclusive = false, message = "Price must be greater than 0") BigDecimal price) {
-        this.price = price;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public void setCoverImage(String coverImage) {
-        this.coverImage = coverImage;
-    }
 }
