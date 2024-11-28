32ae135409ef094f66109179b43220a1cd751619
Bohdan Stratiychuk <vasyahunta@gmail.com>
2024-11-28 13:14:56 +0200

    JWT token

--- src/main/java/com/example/demo/dto/user/UserLoginRequestDto.java
+++ src/main/java/com/example/demo/dto/user/UserLoginRequestDto.java
@@ -1,4 +1,13 @@
 package com.example.demo.dto.user;
 
-public class UserLoginRequestDto {
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public record UserLoginRequestDto(
+        @NotBlank
+        @Email
+        String email,
+        @NotBlank
+        String password
+) {
 }
