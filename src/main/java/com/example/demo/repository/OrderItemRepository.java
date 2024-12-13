9e6e213b5a585a597794e93ef621a2e26c223ca7
Bohdan Stratiychuk <vasyahunta@gmail.com>
2024-12-13 13:57:34 +0200

    checkstyling

--- src/main/java/com/example/demo/repository/OrderItemRepository.java
+++ src/main/java/com/example/demo/repository/OrderItemRepository.java
@@ -3,7 +3,15 @@ package com.example.demo.repository;
 import com.example.demo.model.OrderItem;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
-    Optional<OrderItem> findByIdAndOrderIdAndOrder_User_Id(Long orderId, Long itemId, Long userId);
+    @Query(value = """
+            SELECT oi FROM OrderItem oi
+            JOIN FETCH oi.order o
+            WHERE oi.id = :orderItemId AND o.id = :orderId AND o.user.id = :userId""")
+    Optional<OrderItem> findByIdAndOrderIdAndUserId(@Param("orderItemId") Long orderItemId,
+                                                    @Param("orderId") Long orderId,
+                                                    @Param("userId") Long userId);
 }
